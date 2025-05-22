48f5bc844b7ec9b845774455e82176eb6acbf6ec
Moukaskon <moukaskon04@gmail.com>
2025-05-22 14:27:43 +0300

    Initialize

---
+++ app/src/main/java/com/example/parkpal/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.parkpal;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+
+        loadFragment(new HomeFragment());
+
+        bottomNav.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.nav_home:
+                    loadFragment(new HomeFragment());
+                    return true;
+                case R.id.nav_search:
+                    loadFragment(new SearchFragment());
+                    return true;
+                case R.id.nav_profile:
+                    loadFragment(new ProfileFragment());
+                    return true;
+            }
+            return false;
+        });
+    }
+}
