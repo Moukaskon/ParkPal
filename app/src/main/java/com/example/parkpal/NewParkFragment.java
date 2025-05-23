1b2a4a88f1361e6c1070b63afaba2880dc4ab46e
Moukaskon <moukaskon04@gmail.com>
2025-05-23 14:04:32 +0300

    Add New PArk feature

---
+++ app/src/main/java/com/example/parkpal/NewParkFragment.java
@@ -0,0 +1,20 @@
+package com.example.parkpal;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class NewParkFragment extends Fragment {
+    public NewParkFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_park, container, false);
+    }
+}
