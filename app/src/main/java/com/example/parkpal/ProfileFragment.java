e903632dcd2a07ed5d8dd53134559a3198a0fa46
Theodoros-Moavinis <ics23054@uom.edu.gr>
2025-05-28 22:10:01 +0300

    adapt

--- app/src/main/java/com/example/parkpal/ProfileFragment.java
+++ app/src/main/java/com/example/parkpal/ProfileFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
@@ -15,6 +16,16 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        // code to go to the user wallet fragment
+        Button goToWallet = view.findViewById(R.id.goToWalletButton);
+        goToWallet.setOnClickListener(v -> {
+            // Use the parent activity's method to replace the fragment
+            if (getActivity() instanceof MainActivity) {
+                ((MainActivity) getActivity()).loadFragment(new WalletFragment());
+            }
+        });
+        return view;
     }
 }
--- app/src/main/java/com/example/parkpal/WalletFragment.java
+++ app/src/main/java/com/example/parkpal/WalletFragment.java
@@ -1,4 +1,65 @@
 package com.example.parkpal;
 
-public class WalletFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class WalletFragment extends Fragment {
+
+	@Override
+	public View onCreateView (LayoutInflater inflater, ViewGroup container,
+							  Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_wallet, container, false);
+
+		Button btnRecharge = view.findViewById(R.id.rechargeCreditBTN);
+
+		btnRecharge.setOnClickListener(v -> {
+			if (getActivity() instanceof MainActivity) {
+				((MainActivity) getActivity()).loadFragment(new UserRechargeFragment());
+			}
+		});
+
+
+		TextView balanceText = view.findViewById(R.id.remainingCreditTXT);
+
+		// thread to get the users balance form the server
+		// so that it does not freeze the UI
+		new Thread(() -> {
+			BalanceRequest balanceRequest = new BalanceRequest();
+			String response = balanceRequest.getBalance("http://192.168.1.12/ParkPall/", ((MainActivity) getActivity()).getCurrentUsername());
+
+
+			try {
+				JSONObject json = new JSONObject(response);
+				if (json.getString("status").equals("success")) {
+					double balance = json.getDouble("balance");
+					requireActivity().runOnUiThread(() ->
+							balanceText.setText("" + balance + "$"));
+				} else {
+					requireActivity().runOnUiThread(() ->
+					{
+						try {
+							balanceText.setText("Error: " + json.getString("message"));
+						} catch (JSONException e) {
+							throw new RuntimeException(e);
+						}
+					});
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				requireActivity().runOnUiThread(() ->
+						balanceText.setText("Error parsing balance"));
+			}
+		}).start();
+
+		return view;
+	}
 }
